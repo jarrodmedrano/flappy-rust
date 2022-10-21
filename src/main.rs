7e3cfe7fa276df15229060d589462cd07934d5db
Jarrod Medrano <jarrod.medrano@changehealthcare.com>
2022-10-21 13:40:29 -0600

    remove comments, add enum for modes

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,24 @@
 use bracket_lib::prelude::*;
 
-// START: use
-use bracket_lib::prelude::*;
-// END: use
+enum GameMode {
+    Menu,
+    Playing,
+    End,
+}
 
-// START: state
 struct State {}
-// END: state
 
-// START: gamestate
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         ctx.cls();
         ctx.print(1, 1, "Hello, Bracket Terminal!");
     }
 }
-// END: gamestate
 
-// START: main
-
-// START: mainfn
 fn main() -> BError {
-// END: mainfn
-
-// START: builder
     let context = BTermBuilder::simple80x50()
         .with_title("Flappy Dragon")
         .build()?;
-// END: builder
 
-// START: mainloop
     main_loop(context, State{})
-// END: mainloop
 }
-// END: main
