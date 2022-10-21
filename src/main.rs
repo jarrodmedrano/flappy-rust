3e98d5425385a07f0f04aacf82d27b3e779fb6ad
Jarrod Medrano <jarrod.medrano@changehealthcare.com>
2022-10-21 13:37:36 -0600

    add hello world

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,36 @@
 use bracket_lib::prelude::*;
 
-fn main() {
-    println!("Hello, world!");
-}
+// START: use
+use bracket_lib::prelude::*;
+// END: use
 
+// START: state
+struct State {}
+// END: state
+
+// START: gamestate
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         ctx.cls();
         ctx.print(1, 1, "Hello, Bracket Terminal!");
     }
 }
+// END: gamestate
+
+// START: main
+
+// START: mainfn
+fn main() -> BError {
+// END: mainfn
+
+// START: builder
+    let context = BTermBuilder::simple80x50()
+        .with_title("Flappy Dragon")
+        .build()?;
+// END: builder
+
+// START: mainloop
+    main_loop(context, State{})
+// END: mainloop
+}
+// END: main
